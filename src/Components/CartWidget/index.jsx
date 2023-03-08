4ca2f0db6c963a09d2d1f9bb19c4dd71e095d4c9
Lautaro Gonzalez <lauugonzalez02@gmail.com>
2023-03-08 20:04:00 -0300

    Component ItemCount y mejoras en CartWidget

--- src/Components/CartWidget/index.jsx
+++ src/Components/CartWidget/index.jsx
@@ -1,10 +1,11 @@
 import Nav from 'react-bootstrap/Nav';
+import { Badge } from 'react-bootstrap';
 
 function CartWidget() {
 	return (
 		<Nav.Link href="#cart">
 			<i className="bi bi-cart-fill"></i>
-			<span>1</span>
+			<Badge bg="black">1</Badge>
 		</Nav.Link>
 	);
 }
---
+++ src/Components/ItemCount/index.jsx
@@ -0,0 +1,34 @@
+import { useEffect, useState } from 'react';
+import Button from 'react-bootstrap/Button';
+import Stack from 'react-bootstrap/Stack';
+
+function ItemCount({ stock, initial, onAdd }) {
+	const [count, setCount] = useState(Number(initial));
+	const [quantity, setQuantity] = useState(Number(initial));
+
+	useEffect(() => {}, [count]);
+
+	const sumQuantity = () => {
+		quantity < stock ? setQuantity(quantity + 1) : setQuantity(Number(stock));
+		quantity < stock ? setCount(count + 1) : setCount(Number(stock));
+	};
+
+	const substractQuantity = () => {
+		quantity > 1 ? setQuantity(quantity - 1) : setQuantity(1);
+		quantity > 1 ? setCount(count - 1) : setCount(1);
+	};
+
+	return (
+		<div className="">
+			<Stack direction="horizontal" gap="3">
+				<Button onClick={substractQuantity}>-</Button>
+				<span>{quantity}</span>
+				<Button onClick={sumQuantity}>+</Button>
+			</Stack>
+
+			<Button id="addToCart">Agregar al carrito</Button>
+		</div>
+	);
+}
+
+export default ItemCount;
